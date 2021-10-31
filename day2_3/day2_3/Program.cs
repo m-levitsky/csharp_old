38241e2fa0029463c449119bc042a72d2977aae3
Misha <qwertymaden@gmail.com>
2021-10-31 15:51:10 +0200

    Add files via upload

---
+++ day2_3/day2_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace day2_3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            int[,] a = new int[5, 5];
+
+            Random r = new Random();
+
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                    a[i, j] = r.Next(10, 99);
+
+          
+
+            for (int i = 0; i < 5; i++) a[i, i] = 0; // 0 на главной диагонали
+
+                Console.WriteLine("________________________");
+
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+
+                    Console.Write("{0} ", a[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
